015a50cfee53c34da26d33a2a68868c474ac3163
kdang0 <73298064+kdang0@users.noreply.github.com>
2022-10-01 22:08:12 -0400

    implemented session

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__) 
+app.secret_key = "MAKE IT MAKE SENSE" 
 
 @app.route('/')         
 def index():
@@ -9,11 +10,25 @@ def index():
 def checkout():
     print(request.form)
     print(f'Charging {request.form["first_name"]} for {int(request.form["strawberry"]) + int(request.form["apple"]) + int(request.form["raspberry"])}')
-    return render_template("checkout.html")
+    session["first_name"] = request.form["first_name"]
+    session["last_name"] = request.form["last_name"]
+    session["student_id"] = request.form["student_id"]
+    session["apple"] = request.form["apple"]
+    session["raspberry"] = request.form["raspberry"]
+    session["strawberry"] = request.form["strawberry"]
+    return redirect("/show")
 
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
+
+
+@app.route('/show')
+def show_form():
+    print("Showing this form")
+    print(request.form)
+    return render_template("checkout.html", first_name = session["first_name"],
+    last_name = session["last_name"], student_id = session["student_id"], apple =session["apple"], raspberry =session["raspberry"], strawberry=session["strawberry"])
 
 if __name__=="__main__":   
     app.run(debug=True)    
